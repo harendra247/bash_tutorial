d1ac25e980bffa40ee5d156f67939eb6dfd14d35
Harendra <16508543+harendra247@users.noreply.github.com>
2019-08-07 23:07:54 +0530

    Create bash_script.sh

---
+++ bash_script.sh
@@ -0,0 +1,126 @@
+#!/bin/bash 
+#!/usr/bin/env bash
+# Shebang
+source ./file.sh. # source a file
+. ./file #source a file another way
+enable
+compgen -k 
+alias ll=“ls -l”
+unalias #unset alias
+export
+unset 
+seq 1 9
+echo {A..Z}
+time
+watch
+timeout
+export A=b or declare -x A=b
+export -f function_name
+Function name{
+//command
+#return
+#exit
+}
+declare -l lowercase=“aVV”
+declare -u uppercase=“aVV”
+declare -r readonly=“aVV”
+declare -n integer=“aVV”
+declare -a int_index_array
+declare -A string_index_array
+local a #local variable in side a function
+typeset a #local to a function
+let a #let allows convenient arithmetic
+
+#file handling
+0: in 1: out 2:err
+Command > stdout_here 2> stirrer-here < stdin-here
+Command > file #out redirect to file
+Command 2> file #err redirect to file
+Command &> file #input and err redirect to file
+Command | command2 # commnd2’s stdin coming from command’s stdout
+Command 2>&1 | command2 # commnd2’s gets stdin as stderr stdout from command
+Command |& command2 # commnd2’s gets stdin as stderr stdout from command
+
+
+exec n<file1. #open file with descriptor n for reading
+exec n>file2 # open file with descriptor n for writing
+exec n<>file2 # open file with descriptor n for reading and writing
+exec n>&- or exec n<&-  # close file
+lsof -p $$ # $$ is shell’s PID, lsof list open fds
+ 
+#input to command
+1.
+Command <<END
+Comment lien one
+Comment line 2
+Comment line 3
+END
+
+2.
+<<COMMENT
+Comment lien one
+Comment line 2
+Comment line 3
+COMMENT
+
+2.
+:’
+Comment lien one
+Comment line 2
+Comment line 3
+‘
+
+#test command
+test -x /bin/ls
+
+Filters > head, tail, wc, sed, awk
+Script parameters and {}
+$1 $2 #variables are 1 and 2
+${abc} #variable name is abc
+x=abc
+abc=yz
+echo ${!x}. # prints yz …redirection!
+${variable <operator> value}
+x=${var:-hotdog}
+
+:- if var is unset/null return value otherwise value of var
+:=  if var is unset/null set var with value and return value otherwise value of var
+:?  if var is unset/null then it displays error and exits script
+:+  if var is unset/null then it returns nothing
+
+String operations
+${var:offset} # value of var staring from offset
+${var:offset:len} # value of var staring from offset up to Len
+${#var} # length of var
+${var#pre} # remove matching prefix
+${var%post} # remove matching suffix
+
+
+tr : translate command
+
+Coproceses : runs in background and gives fd for read write
+coproc ./mycoproc.sh 
+
+echo banana >& “${COPROC[1]}”
+cat <& “${COPROC[0]}”
+
+coproc my { ./mycoproc.sh }.  # coprocessor with name my
+echo banana >& “${my[1]}”
+cat <& “${my[0]}”
+
+Debugging script
+
+bash prog # no executable permission is required
+bash -x prog #execute command after processing can also do set -x pr set +x inside of script
+bash -n prog #no execute command do syntax check
+set -u # report usage of unset variable
+
+Trap: using signals
+
+trap “just got int exit” INT
+trap “u can’t quit” QUIT 
+
+Eval command #http://mywiki.wooledge.org/BashFAQ/048
+Getopt: process command line input
+
+opts=`getopt -o a: -l apple — “$@”`
